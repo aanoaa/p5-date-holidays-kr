2262b6c0da0f58b86eff12d59724d38e3c33e926
Hyungsuk Hong <aanoaa@gmail.com>
2016-01-05 00:46:45 +0900

    Implements is_kr_holiday and kr_holidays subroutines

--- lib/Date/Holidays/KR.pm
+++ lib/Date/Holidays/KR.pm
@@ -2,9 +2,9 @@ package Date::Holidays::KR;
 use utf8;
 use strict;
 use warnings;
-use base qw/Exporter/;
+use base qw(Exporter);
 use version; our $VERSION = version->declare("v0.0.1");
-our @EXPORT = qw/is_kr_holiday/;
+our @EXPORT = qw(is_kr_holiday kr_holidays);
 
 =head1 NAME
 
@@ -27,31 +27,52 @@ Date::Holidays::KR - Determine KR Public Holidays
 Returns the name of the Holiday that falls on the given day, or undef
 if there is none.
 
+=head2 kr_holidays( $year )
+
+Returns hashref contains whole holidays in C<$year>, each keys are
+concatenate string of 2 digit month and 2 digit day
+
+    my $holidays = kr_holidays(2016);
+    ## 25th of December is annual Christmas
+    print $holidays->{'1225'};    # 기독탄신일
+
 =cut
 
 our %holidays;
 
-$holidays{ 2015, 1,  1 }  = $holidays{ 2016, 1,  1 }  = '신정';
-$holidays{ 2015, 2,  18 } = $holidays{ 2016, 2,  7 }  = '설날 전날';
-$holidays{ 2015, 2,  19 } = $holidays{ 2016, 2,  8 }  = '설날';             # 음력 1월 1일
-$holidays{ 2015, 2,  20 } = $holidays{ 2016, 2,  9 }  = '설날 다음날';
-$holidays{ 2015, 3,  1 }  = $holidays{ 2016, 3,  1 }  = '3·1절';
-$holidays{ 2015, 5,  5 }  = $holidays{ 2016, 5,  5 }  = '어린이날';
-$holidays{ 2015, 5,  25 } = $holidays{ 2016, 5,  14 } = '석가탄신일';    # 음력 4월 8일
-$holidays{ 2015, 6,  6 }  = $holidays{ 2016, 6,  6 }  = '현충일';
-$holidays{ 2015, 7,  17 } = $holidays{ 2016, 7,  17 } = '제헌절';
-$holidays{ 2015, 8,  15 } = $holidays{ 2016, 8,  15 } = '광복절';
-$holidays{ 2015, 9,  26 } = $holidays{ 2016, 9,  14 } = '추석 전날';
-$holidays{ 2015, 9,  27 } = $holidays{ 2016, 9,  15 } = '추석';             # 음력 8월 15일
-$holidays{ 2015, 9,  28 } = $holidays{ 2016, 9,  16 } = '추석 다음날';
-$holidays{ 2015, 9,  29 } = '추석 대체휴일';
-$holidays{ 2015, 10, 3 }  = $holidays{ 2016, 10, 3 }  = '개천절';
-$holidays{ 2015, 10, 9 }  = $holidays{ 2016, 10, 9 }  = '한글날';
-$holidays{ 2015, 12, 25 } = $holidays{ 2016, 12, 25 } = '기독탄신일';
+$holidays{'2015'}{'01'}{'01'} = $holidays{'2016'}{'01'}{'01'} = '신정';
+$holidays{'2015'}{'02'}{'18'} = $holidays{'2016'}{'02'}{'07'} = '설날전날';
+$holidays{'2015'}{'02'}{'19'} = $holidays{'2016'}{'02'}{'08'} = '설날';             #음력1월1일
+$holidays{'2015'}{'02'}{'20'} = $holidays{'2016'}{'02'}{'09'} = '설날다음날';
+$holidays{'2015'}{'03'}{'01'} = $holidays{'2016'}{'03'}{'01'} = '3·1절';
+$holidays{'2015'}{'05'}{'05'} = $holidays{'2016'}{'05'}{'05'} = '어린이날';
+$holidays{'2015'}{'05'}{'25'} = $holidays{'2016'}{'05'}{'14'} = '석가탄신일';    #음력4월8일
+$holidays{'2015'}{'06'}{'06'} = $holidays{'2016'}{'06'}{'06'} = '현충일';
+$holidays{'2015'}{'07'}{'17'} = $holidays{'2016'}{'07'}{'17'} = '제헌절';
+$holidays{'2015'}{'08'}{'15'} = $holidays{'2016'}{'08'}{'15'} = '광복절';
+$holidays{'2015'}{'09'}{'26'} = $holidays{'2016'}{'09'}{'14'} = '추석전날';
+$holidays{'2015'}{'09'}{'27'} = $holidays{'2016'}{'09'}{'15'} = '추석';             #음력8월15일
+$holidays{'2015'}{'09'}{'28'} = $holidays{'2016'}{'09'}{'16'} = '추석다음날';
+$holidays{'2015'}{'09'}{'29'} = '추석대체휴일';
+$holidays{'2015'}{'10'}{'03'} = $holidays{'2016'}{'10'}{'03'} = '개천절';
+$holidays{'2015'}{'10'}{'09'} = $holidays{'2016'}{'10'}{'09'} = '한글날';
+$holidays{'2015'}{'12'}{'25'} = $holidays{'2016'}{'12'}{'25'} = '기독탄신일';
 
 sub is_kr_holiday {
     my ( $year, $month, $day ) = @_;
-    return $holidays{ $year, $month, $day };
+    return $holidays{$year}{ sprintf '%02d', $month }{ sprintf '%02d', $day };
+}
+
+sub kr_holidays {
+    my ($year) = @_;
+    my $h = {};
+    for my $month ( keys %{ $holidays{$year} || {} } ) {
+        for my $day ( keys %{ $holidays{$year}{$month} || {} } ) {
+            $h->{ sprintf '%02d%02d', $month, $day } = $holidays{$year}{$month}{$day};
+        }
+    }
+
+    return $h;
 }
 
 1;
@@ -65,7 +86,7 @@ http://www.nodong.or.kr/holyday/403063
 
 =head1 SEE ALSO
 
-L<Date::Holidays::UK>
+L<Date::Holidays::UK>, L<Date::Holidays::DK>
 
 =head1 COPYRIGHT
 
